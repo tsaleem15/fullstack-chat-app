9293ef74c7c796298223676c74447054bf371237
Talha Saleem <86004377+tsaleem15@users.noreply.github.com>
2025-08-01 06:51:32 +0500

    Project Completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const connectDB = async ()=>{
+
+    try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`MongoDB connect: ${conn.connection.host}`)
+
+    } catch (error){
+
+        console.log("MongoDB connection error:",error)
+
+
+    }
+}
